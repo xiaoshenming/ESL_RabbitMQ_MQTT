5b73085797bd6869d2a544fcc6afcfce91c567ab
chenProfession <ccelephant2012@gmail.com>
2025-07-11 11:55:03 +0800

    download example

---
+++ src/main/java/com/pandatech/downloadcf/DownloadcfApplication.java
@@ -0,0 +1,13 @@
+package com.pandatech.downloadcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadcfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownloadcfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pandatech/downloadcf/DownloadcfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandatech.downloadcf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadcfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
